9f97edaabfb2f03efb51aa887c025018a7fd6aab
arthropodas <asifnp1010@gmail.com>
2024-11-06 22:37:42 +0530

    implemented useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import RestaurantCard from "./Restaurant";
 import resList from "../utils/mockData";
 function Body() {
+  const [newArray, setNewArray] = useState(resList);
   return (
     <div>
       <div className="search">
         <input placeholder="search food" />
         <button className="search-btn">search</button>
       </div>
-      
+      <button onClick={
+        ()=>{
+           const filterRes = newArray.filter((restaurant)=>(restaurant.info.avgRating>4.7)) 
+           setNewArray(filterRes)
+        }
+      } className="filter-btn">filter high rated</button>
 
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {newArray.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
