8d04ff46740741ad3e60466f2da060afd52c8655
arthropodas <asifnp1010@gmail.com>
2024-11-11 11:17:00 +0530

    added lazy loading in code refactoring

---
+++ src/utils/hooks/useIsOnline.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+function useIsOnline() {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    // Add event listeners for online/offline events
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  return isOnline;
+}
+
+export default useIsOnline;
